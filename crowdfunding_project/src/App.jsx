8e7e28fb10e61050e82387bb20c83cc9cf9eb7b8
XShatta <newhitchcock@yahoo.com>
2025-07-04 00:09:59 +0300

    fixed some syntax error

--- crowdfunding_project/src/App.jsx
+++ crowdfunding_project/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import route from './routes/RouteList'
 import { RouterProvider } from 'react-router-dom'
--- crowdfunding_project/src/pages/NotFoundPage.jsx
+++ crowdfunding_project/src/pages/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
   return (
     <>
     <div className=''>
-        <h3 className='py-5'>Not fo Content</h3>
+        <h3 className='py-5'>Not found Content</h3>
         <p className='mb-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores explicabo nobis laborum ipsa, ullam animi magni similique quos adipisci dolorem!</p>
                <Link className='ms-3' to={'/'}>back to home</Link>
 
